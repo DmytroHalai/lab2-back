84cfb974c2dd2178a4a3ab0f3f1d8878c5b6d14f
DmytroHalai <dimagalai04@gmail.com>
2025-10-14 17:49:21 +0300

    Initial commit

---
+++ src/main/java/org/example/lab2back/Lab2BackApplication.java
@@ -0,0 +1,13 @@
+package org.example.lab2back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab2BackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab2BackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/lab2back/Lab2BackApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.lab2back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab2BackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
