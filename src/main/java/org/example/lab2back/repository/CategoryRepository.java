c570fd565b1a43b942ae8995eccb4f9656917d80
DmytroHalai <dimagalai04@gmail.com>
2025-10-21 19:31:53 +0300

    Refactor CategoryEntity and CategoryRepository to use JPA for improved data management

--- src/main/java/org/example/lab2back/repository/CategoryRepository.java
+++ src/main/java/org/example/lab2back/repository/CategoryRepository.java
@@ -1,42 +1,12 @@
 package org.example.lab2back.repository;
 
-import jakarta.annotation.PostConstruct;
 import jakarta.persistence.EntityNotFoundException;
 import org.example.lab2back.entity.CategoryEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
-import static org.example.lab2back.bd.Initializer.setTestCategories;
-
 @Repository
-public class CategoryRepository {
-    public List<CategoryEntity> categories;
-
-    public CategoryRepository(List<CategoryEntity> categories) {
-        this.categories = categories;
-    }
-
-    public void save(CategoryEntity entity) {
-        categories.add(entity);
-    }
-
-    @PostConstruct
-    public void init() {
-        categories.addAll(setTestCategories());
-    }
-
-    public List<CategoryEntity> findAll() {
-        return categories;
-    }
-
-    public Optional<CategoryEntity> findById(Object id) {
-        return categories.stream().filter(category -> category.getId().equals(id)).findFirst();
-    }
-
-    public void deleteById(Object id) {
-        if (!categories.removeIf(category -> category.getId().equals(id)))
-            throw new EntityNotFoundException("Category by id: " + id + " not found");
-    }
-}
+public interface CategoryRepository extends JpaRepository<CategoryEntity, Long> {}
