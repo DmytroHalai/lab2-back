304a038457fbfdd8526f45fc66f1ea810418925d
DmytroHalai <dimagalai04@gmail.com>
2025-10-25 19:38:26 +0300

    Change Service logic due to relations changes

--- src/main/java/org/example/lab2back/service/RecordService.java
+++ src/main/java/org/example/lab2back/service/RecordService.java
@@ -1,6 +1,9 @@
 package org.example.lab2back.service;
 
+import jakarta.persistence.EntityNotFoundException;
+import org.example.lab2back.entity.CategoryEntity;
 import org.example.lab2back.entity.RecordEntity;
+import org.example.lab2back.entity.UserEntity;
 import org.example.lab2back.repository.CategoryRepository;
 import org.example.lab2back.repository.RecordRepository;
 import org.example.lab2back.repository.UserRepository;
@@ -21,31 +24,40 @@ public class RecordService {
     }
 
     public List<RecordEntity> getRecordsByUserIdAndCategoryId(Long userId, Long categoryId) {
-        if (userId != null && categoryId != null) return repository.getRecordsByUserIdAndCategoryId(userId, categoryId);
-        else if (userId != null) return repository.getRecordsByUserId(userId);
-        else if (categoryId != null) return repository.getRecordsByCategoryId(categoryId);
-        else throw new IllegalArgumentException("At least one parameter must be provided");
+        if (userId == null && categoryId == null)
+            throw new IllegalArgumentException("At least one parameter must be provided");
+        if (userId != null && !userRepository.existsById(userId)) throw new EntityNotFoundException("User not found");
+        if (categoryId != null && !categoryRepository.existsById(categoryId))
+            throw new EntityNotFoundException("Category not found");
+        if (userId != null && categoryId != null) return repository.findByUserIdAndCategoryId(userId, categoryId);
+        return userId != null
+                ? repository.findByUserId(userId)
+                : repository.findByCategoryId(categoryId);
     }
 
     public RecordEntity getRecordById(Long id) {
         return repository
-                .getRecordById(id)
-                .orElseThrow(() -> new IllegalArgumentException("Record by id: " + id + " not found"));
+                .findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("Record by id: " + id + " not found"));
     }
 
     public void deleteRecordById(Long id) {
-        repository.deleteRecordById(id);
+        if (!repository.existsById(id)) throw new EntityNotFoundException("Record not found");
+        repository.deleteById(id);
     }
 
-    public RecordEntity createRecord(RecordEntity oneRecord) {
-        if (checkIsValid(oneRecord)) throw new IllegalArgumentException("Input ids are not valid");
-        RecordEntity newRecord = new RecordEntity(oneRecord.getCategoryId(), oneRecord.getUserId(), oneRecord.getAmount());
-        repository.save(newRecord);
-        return newRecord;
+    public RecordEntity createRecord(RecordEntity oneRecord, Long userId, Long categoryId) {
+        UserEntity user = userRepository.findById(userId)
+                .orElseThrow(() -> new EntityNotFoundException("User not found"));
+        CategoryEntity category = categoryRepository.findById(categoryId)
+                .orElseThrow(() -> new EntityNotFoundException("Category not found"));
+        if (!category.getUser().getId().equals(userId))
+            throw new IllegalArgumentException("Category doesn't belong to user");
+
+        RecordEntity newRecord = new RecordEntity(oneRecord.getAmount());
+        newRecord.setUser(user);
+        newRecord.setCategory(category);
+        return repository.save(newRecord);
     }
 
-    private boolean checkIsValid(RecordEntity oneRecord) {
-        return userRepository.findById(oneRecord.getUserId()).isEmpty() ||
-                categoryRepository.findById(oneRecord.getCategoryId()).isEmpty();
-    }
 }
