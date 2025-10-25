ba8fe7ac4c6743bc4470b9adab9ef12c7adeddf2
DmytroHalai <dimagalai04@gmail.com>
2025-10-25 19:34:01 +0300

    Change dependency between entities.
    Now they are inn relations like:
    user has many categories
    category has many records
    category can't be without user
    record can't be without category
    record can't be without user

--- src/main/java/org/example/lab2back/entity/UserEntity.java
+++ src/main/java/org/example/lab2back/entity/UserEntity.java
@@ -1,15 +1,14 @@
 package org.example.lab2back.entity;
 
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import jakarta.validation.constraints.Size;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
-@AllArgsConstructor
-@NoArgsConstructor
 public class UserEntity {
 
     @Id
@@ -17,14 +16,32 @@ public class UserEntity {
     private Long id;
 
     @Column(nullable = false)
+    @NotBlank
+    @Size(min = 2, max = 50, message = "Ім'я має містити від 2 до 50 символів")
     private String username;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    @JsonManagedReference
+    private List<CategoryEntity> categories = new ArrayList<>();
 
     public UserEntity(String username) {
         this.username = username;
     }
 
+    public UserEntity() {
+
+    }
+
     public Long getId() {
         return id;
+    }
+
+    public List<CategoryEntity> getCategories() {
+        return categories;
+    }
+
+    public void setCategories(List<CategoryEntity> categories) {
+        this.categories = categories;
     }
 
     public void setId(Long id) {
