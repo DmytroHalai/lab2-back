28d306ca50962fc48eacaefc6681d5ea80d19ffc
DmytroHalai <dimagalai04@gmail.com>
2025-10-25 19:34:37 +0300

    Fix category entity

--- src/main/java/org/example/lab2back/entity/CategoryEntity.java
+++ src/main/java/org/example/lab2back/entity/CategoryEntity.java
@@ -1,38 +1,68 @@
 package org.example.lab2back.entity;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
-import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
+import jakarta.validation.constraints.Size;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
-@NoArgsConstructor
-@AllArgsConstructor
 public class CategoryEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(nullable = false, unique = true)
+    @NotBlank
+    @Size(min = 2, max = 50, message = "Ім'я має містити від 2 до 50 символів")
     private String name;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    @JsonBackReference
+    private UserEntity user;
+    @OneToMany(mappedBy = "category", cascade = CascadeType.REMOVE, orphanRemoval = true)
+    private List<RecordEntity> records = new ArrayList<>();
 
     public CategoryEntity(String name) {
         this.name = name;
     }
 
-    public void setId(Long id) {
-        this.id = id;
+    public CategoryEntity() {
+
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public UserEntity getUser() {
+        return user;
+    }
+
+    public void setUser(UserEntity user) {
+        this.user = user;
+    }
+
+    public List<RecordEntity> getRecords() {
+        return records;
+    }
+
+    public void setRecords(List<RecordEntity> records) {
+        this.records = records;
     }
 
     public Long getId() {
         return id;
     }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
+
     public String getName() {
         return name;
     }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
 }
