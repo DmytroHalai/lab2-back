eb4e936b2fb73b1ef140a5609c066b6466c0ea68
DimaHalai <dimagalai04@gmail.com>
2025-11-10 18:09:30 +0200

    Update DTOs

--- src/main/java/org/example/lab2back/dto/AuthRequest.java
+++ src/main/java/org/example/lab2back/dto/AuthRequest.java
@@ -1,23 +1,17 @@
 package org.example.lab2back.dto;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
 
-public class UserCreateDto {
+public class AuthRequest {
     @NotBlank
-    @Size(min = 2, max = 50)
     private String username;
 
     @NotBlank
     private String password;
-
-    private String currency;
 
     // getters & setters
     public String getUsername() { return username; }
     public void setUsername(String username) { this.username = username; }
     public String getPassword() { return password; }
     public void setPassword(String password) { this.password = password; }
-    public String getCurrency() { return currency; }
-    public void setCurrency(String currency) { this.currency = currency; }
 }
